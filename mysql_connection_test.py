0c5149ecab12858d315a33beb2ae383638a0691d
justforwhimsy <pomoluese@gmail.com>
2017-07-14 23:13:27 -0700

    compiled database of counties - has not been reviewed yet

--- mysql_connection_test.py
+++ mysql_connection_test.py
@@ -1,20 +1,19 @@
 import _mysql
 
-db = _mysql.connect("localhost", "user", "password", "public_county_pages")
+db = _mysql.connect("localhost", "USER", "PASSWORD", "DB")
 
 
-
-new_row = """insert into fb_pages (id, page_name, page_url, page_state, page_county) Values("1234552", "Test", "http://test.com", 'ts', 'test county') """
+# new_row = """insert into fb_pages (id, page_name, page_url, page_state, page_county) Values("1234552", "Test", "http://test.com", 'ts', 'test county') """
 # db.query(new_row)
-page_id = "1234553"
-page_name ="Test"
-page_url = "http://test.com"
-page_state = "OR"
-page_county = "Multnomah County"
-query = """insert into fb_pages (id, page_name, page_url, page_state, page_county) Values('%s', '%s', '%s', '%s', '%s')""" % (page_id, page_name, page_url, page_state, page_county)
-db.query(query)
-content = db.query("""select * from fb_pages""")
+# page_id = "1234553"
+# page_name ="Test"
+# page_url = "http://test.com"
+# page_state = "OR"
+# page_county = "Multnomah County"
+# query = """insert into fb_pages (id, page_name, page_url, page_state, page_county) Values('%s', '%s', '%s', '%s', '%s')""" % (page_id, page_name, page_url, page_state, page_county)
+# db.query(query)
+# content = db.query("""select * from fb_pages""")
 
-results = db.use_result()
+# results = db.use_result()
 
-print(results.fetch_row(0))
+# print(results.fetch_row(0))
